6324a0a8d59bdf07bcaac5cdcde78d23901f4b3f
Ashish <Ashish@192.168.0.6>
2017-04-28 19:17:19 -0600

    initial Commit!

---
+++ Hibernate/src/com/hibernate/demo/HibernateDemo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.hibernate.demo;
+
+/**
+ * @author Ashish
+ *
+ */
+public class HibernateDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hibernate Initial Commit!");
+		
+	}
+
+}
